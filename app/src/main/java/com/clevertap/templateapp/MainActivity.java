ce09e95539eb7d1bc08faed691f2798a62957f14
Vijay <vijay.agnihotri@clevertap.com>
2020-05-19 19:24:47 +0530

    Removing test code

--- app/src/main/java/com/clevertap/templateapp/MainActivity.java
+++ app/src/main/java/com/clevertap/templateapp/MainActivity.java
@@ -9,11 +9,6 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.clevertap.android.sdk.CleverTapAPI;
-import com.clevertap.pushtemplates.TemplateRenderer;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -44,31 +39,9 @@ public class MainActivity extends AppCompatActivity {
         sendBasicPush.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*if (cleverTapAPI != null) {
+                if (cleverTapAPI != null) {
                     cleverTapAPI.pushEvent("Send Basic Push");
-                }*/
-                Bundle extras = new Bundle();
-
-                try {
-                    JSONObject payload = new JSONObject("{\"wzrk_pid\":\"2\",\"wzrk_cid\":\"PTTesting\",\"pt_id\":\"pt_basic\",\"pt_msg\": \"Now Streaming Live!\",\"pt_msg_summary\": \"Get all these just for $10 each.\",\n" +
-                            "    \"pt_title\": \"Catch The Avengers\",\n" +
-                            "    \"pt_msg_clr\": \"#000000\",\n" +
-                            "    \"pt_title_clr\": \"#000000\",\n" +
-                            "    \"pt_big_img\": \"https://www.bmmagazine.co.uk/wp-content/uploads/2020/03/marvel-avengers-not-ended-disney-ceo-teases-mcu-fans.jpg\",\n" +
-                            "    \"pt_ico\": \"https://i.pinimg.com/originals/49/3e/de/493ede620ab04894295105635d73f77d.png\"," +
-                            "    \"pt_dl1\": \"https://google.com\"}");
-                    //JSONObject payload = new JSONObject("{\"wzrk_cid\":\"PTTesting\",\"pt_id\":\"3\",\"pt_msg\":\"Please share your rating below.\",\"pt_title\":\"Your feedback is important.\",\"pt_small_img\":\"https://d35fo82fjcw0y8.cloudfront.net/2019/12/02015204/Manan-Bajoria.png\",\"pt_dl1\":\"https:///www.google.com\"}");
-
-                    JSONArray keys = payload.names();
-                    for (int i = 0; i < keys.length(); ++i) {
-                        String key = keys.getString(i);
-                        String value = payload.getString(key);
-                        extras.putString(key, value);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
-                TemplateRenderer.createNotification(getApplicationContext(),extras);
             }
         });
 
