ea0397375604b04d2542d67167f7625fe488d178
Vijay <vijay.agnihotri@clevertap.com>
2020-05-24 15:40:25 +0530

    Adding manual carousel for 3 images

--- app/src/main/java/com/clevertap/templateapp/MyApplication.java
+++ app/src/main/java/com/clevertap/templateapp/MyApplication.java
@@ -4,11 +4,13 @@ import android.app.Application;
 
 import com.clevertap.android.sdk.ActivityLifecycleCallback;
 import com.clevertap.android.sdk.CleverTapAPI;
+import com.clevertap.pushtemplates.TemplateRenderer;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         CleverTapAPI.setDebugLevel(3);
+        TemplateRenderer.setDebugLevel(3);
         ActivityLifecycleCallback.register(this);
         super.onCreate();
     }
