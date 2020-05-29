a3708246201398958950a45113ad13b745808d0d
divyekant <divyekantg@gmail.com>
2020-05-30 01:45:54 +0530

    Cleaned the manifestinfo file

--- pushtemplates/src/main/java/com/clevertap/pushtemplates/ManifestInfo.java
+++ pushtemplates/src/main/java/com/clevertap/pushtemplates/ManifestInfo.java
@@ -7,20 +7,9 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 class ManifestInfo {
-    private static String accountId;
-    private static String accountToken;
-    private static String accountRegion;
-    private static boolean useADID;
-    private static boolean appLaunchedDisabled;
     private static String notificationIcon;
     private static ManifestInfo instance;
-    private static String excludedActivities;
-    private static boolean sslPinning;
-    private static boolean backgroundSync;
-    private static boolean useCustomID;
     private static String fcmSenderId;
-    private static String packageName;
-    private static boolean beta;
     private static String intentServiceName;
 
     private static String _getManifestStringValueForKey(Bundle manifest, String name) {
@@ -58,61 +47,8 @@ class ManifestInfo {
         return instance;
     }
 
-    String getAccountId(){
-        return accountId;
-    }
-
-    String getAcountToken(){
-        return accountToken;
-    }
-
-    String getAccountRegion(){
-        return accountRegion;
-    }
-
-    String getFCMSenderId() {
-        return fcmSenderId;
-    }
-
-    boolean useGoogleAdId(){
-        return useADID;
-    }
-
-    boolean enableBeta(){
-        return beta;
-    }
-
-    boolean isAppLaunchedDisabled(){
-        return appLaunchedDisabled;
-    }
-
-    boolean isSSLPinningEnabled(){return sslPinning;}
-
-    String getNotificationIcon() {
-        return notificationIcon;
-    }
-
-    String getExcludedActivities(){return excludedActivities;}
-
-    boolean isBackgroundSync() {
-        return backgroundSync;
-    }
-
-    boolean useCustomId(){
-        return useCustomID;
-    }
-
-    String getPackageName() {
-        return packageName;
-    }
-
     String getIntentServiceName (){
         return intentServiceName;
     }
 
-    static void changeCredentials(String id, String token, String region){
-        accountId = id;
-        accountToken = token;
-        accountRegion = region;
-    }
 }
