cf7bce4ba3c95942da7b6016ac4716d05f896d6f
Satyajeet Jadhav <satyajeet@clevertap.com>
2020-07-15 19:05:31 +0530

    small icon bug fix for auto carousel and manual carousel

--- pushtemplates/src/main/java/com/clevertap/pushtemplates/TemplateRenderer.java
+++ pushtemplates/src/main/java/com/clevertap/pushtemplates/TemplateRenderer.java
@@ -724,8 +724,6 @@ public class TemplateRenderer {
 
             Notification notification = notificationBuilder.build();
 
-            Utils.loadIntoGlide(context, R.id.small_icon, pt_large_icon, contentViewSmall, notification, notificationId);
-
             ArrayList<Integer> layoutIds = new ArrayList<>();
             layoutIds.add(0, R.id.flipper_img1);
             layoutIds.add(1, R.id.flipper_img2);
@@ -873,8 +871,6 @@ public class TemplateRenderer {
 
 
             Notification notification = notificationBuilder.build();
-
-            Utils.loadIntoGlide(context, R.id.small_icon, pt_large_icon, contentViewSmall, notification, notificationId);
 
             Utils.loadIntoGlide(context, R.id.carousel_image, imageList.get(0), contentViewManualCarousel, notification, notificationId);
 
